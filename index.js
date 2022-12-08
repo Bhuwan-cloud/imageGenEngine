63ffcdae374fd2101fb0d88333efaa7759b118d2
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2022-12-08 21:14:30 +0545

    frontend completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -5,6 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+//  set static folder 
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/openai', require('./routes/openAIRoutes'))
